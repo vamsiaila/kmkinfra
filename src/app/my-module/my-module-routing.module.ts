a5cc7fcd96667222c7149821c4018887011803d7
VAMSI AILA <35575642+vamsiaila@users.noreply.github.com>
2018-03-31 02:44:54 +0530

    route update

--- src/app/my-module/my-module-routing.module.ts
+++ src/app/my-module/my-module-routing.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from '../app.component';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+
 import { HomeComponent } from '../home/home.component';
 import { AboutusComponent } from '../aboutus/aboutus.component';
 import { AllprojectsComponent } from '../allprojects/allprojects.component';
 import { FpOverviewComponent } from '../fp-overview/fp-overview.component';
-import { SubNavComponent } from '../sub-nav/sub-nav.component';
 import { FpProjectHighlightsComponent } from '../fp-project-highlights/fp-project-highlights.component';
 import { FpLocationMapComponent } from '../fp-location-map/fp-location-map.component';
 import { FpFloorPlansComponent } from '../fp-floor-plans/fp-floor-plans.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutusComponent },
+  { path: 'FeaturedProject', component: FpOverviewComponent },
+  { path: 'AllProjects', component: AllprojectsComponent },
   { path: 'about', component: AboutusComponent },
-  { path: 'products', component: AllprojectsComponent },
-  { path: 'contact', component: FpOverviewComponent }
+  { path: 'about', component: AboutusComponent },
+  { path: 'about', component: AboutusComponent }
   
 ];
 
