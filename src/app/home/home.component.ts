8c898ebed1c23a18631accb4c65d2f7c7decb5ab
VAMSI AILA <35575642+vamsiaila@users.noreply.github.com>
2018-03-31 14:36:09 +0530

    animate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Fade } from '../animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    Fade
+  ]
 })
 export class HomeComponent implements OnInit {
 
